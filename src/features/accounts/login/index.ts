cc95e11c8a5ab46deb326758a9be1b0744f42b35
leegyuhan99 <leegyuhan99@gmail.com>
2026-04-24 14:29:38 +0900

    fix: API baseURL 환경변수 적용에 따른 엔드포인트 경로 수정

--- src/features/accounts/login/index.ts
+++ src/features/accounts/login/index.ts
@@ -2,9 +2,6 @@ import instance from '@/api/instance'
 import type { LoginRequest, LoginResponse } from './types'
 
 export const login = async (data: LoginRequest): Promise<LoginResponse> => {
-  const response = await instance.post<LoginResponse>(
-    '/api/v1/accounts/login',
-    data
-  )
+  const response = await instance.post<LoginResponse>('/accounts/login', data)
   return response.data
 }
